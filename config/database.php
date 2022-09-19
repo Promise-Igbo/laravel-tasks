902d5e626404123748827dc461858e5e717c57db
Promise Igbo <100773978+Promise-Igbo@users.noreply.github.com>
2022-09-19 19:43:13 +0100

    add certificate

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('msdocs-laravel-mysql-970-server.mysql.database.azure.comT', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('msdocs-docs-laravel-mysql-970-database', 'forge'),
+            'username'  => env('tsayjtxdyq', 'forge'),
+            'password'  => env('PJ224HQ812XAH8C0$', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem'),
             ]) : [],
         ],
 
